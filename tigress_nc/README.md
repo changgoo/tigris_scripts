6db5ccbcca163f2c84eab333a8062a1619d3f52d
Chang-Goo Kim <cgkim@astro.princeton.edu>
2025-04-08 17:38:02 -0400

    Added new scripts for TIGRESS-NC(R)

---
+++ tigress_nc/README.md
@@ -0,0 +1,6 @@
+On `stellar`, MHD run with `icpx` at 16pc resolution can be run by
+
+```sh
+./compile.sh icpx mhd
+sbatch tigress_nc_mhd.slurm icpx
+```
---
+++ tigress_nc/compile.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# tigress-classic compilation script works for stellar and tiger3
+#   alias for module_icpx an module_gcc should be in .bashrc,
+#   otherwise uncomment the following
+source ~/.bashrc
+set -e
+
+CC=$1
+if [ "$CC" == "icpx" ] ; then
+    module_icpx
+    debug_option=""
+elif [ "$CC" == "g++" ] ; then
+    module_gcc
+    debug_option=""
+else
+    module_gcc
+    CC=g++
+    debug_option="-debug"
+fi
+echo "CC is set to $CC"
+
+# CC=g++
+echo $HDF5DIR
+mpi_hdf5_library_path="$HDF5DIR/lib64"
+mpi_hdf5_include_path="$HDF5DIR/include"
+
+options="${debug_option} -mpi -hdf5 --cxx=$CC --lib_path=${mpi_hdf5_library_path} --include=${mpi_hdf5_include_path}"
+
+physics=$2
+if [ "$physics" == "hydro" ] ; then
+    phy_options=""
+elif [ "$physics" == "mhd" ] ; then
+    phy_options="-b"
+elif [ "$physics" == "crmhd" ] ; then
+    phy_options="-b --cr=mg"
+else
+    echo "physics is set to hydro"
+    physics=hydro
+fi
+
+current=`pwd`
+SRCDIR=$HOME/tigris/
+cd $SRCDIR
+
+prob=tigress_nc
+./configure.py --prob=$prob --nghost=4 -fft -fb --grav=blockfft --nspecies=3 $options $phy_options
+
+make clean
+make all -j4
+
+cp bin/athena $current/${prob}_${physics}_$CC$debug_option.exe
+
+cd $current
+set +e
